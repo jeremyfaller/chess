527d881fcbbf64072f1c05720b536661e1352303
Jeremy Faller <jeremy.faller@gmail.com>
2022-09-29 15:43:39 -0400

    improve eval benchmarking

--- eval_test.go
+++ eval_test.go
@@ -9,35 +9,39 @@ import (
 )
 
 //go:embed testdata/mate.dat
-var mate string
+var mates string
 
-func TestMateIn(t *testing.T) {
-	// Read the test data.
-	type testT struct {
-		depth int
-		fen   string
-	}
-	var tests []testT
-	for _, str := range strings.Split(mate, "\n") {
+type evalTest struct {
+	depth int
+	fen   string
+}
+
+// getTests returns testTs from the passed in string.
+func getTests(dat string) []evalTest {
+	var tests []evalTest
+	for _, str := range strings.Split(dat, "\n") {
 		if len(str) == 0 {
 			continue
 		}
 		depth, err := strconv.Atoi(str[0:1])
 		if err != nil {
-			t.Fatal(err)
+			panic(err)
 		}
-		tests = append(tests, testT{depth: 2*depth - 1, fen: str[2:]})
+		tests = append(tests, evalTest{depth: 2*depth - 1, fen: str[2:]})
 	}
+	return tests
+}
 
+func TestMateIn(t *testing.T) {
 	// Run the tests.
-	for i, test := range tests {
+	for i, test := range getTests(mates) {
 		test := test
 
 		t.Run(fmt.Sprintf("test %d, mate in %d", i, test.depth), func(t *testing.T) {
 			t.Parallel()
 			b, err := FromFEN(test.fen)
 			if err != nil {
-				t.Fatalf("[%d] error on fen %v", i, err)
+				t.Fatalf("[%d] error in fen %v", i, err)
 			}
 
 			e := NewEval(b, test.depth)
@@ -47,4 +51,31 @@ func TestMateIn(t *testing.T) {
 			}
 		})
 	}
+}
+
+func mateBenchmarker(b *testing.B, d int, tests []evalTest) {
+	for j := 0; j < b.N; j++ {
+		for i, test := range getTests(mates) {
+			if test.depth != d {
+				continue
+			}
+			b, err := FromFEN(test.fen)
+			if err != nil {
+				panic(fmt.Sprintf("[%d] error in fen %v", i, err))
+			}
+			e := NewEval(b, test.depth)
+			e.Start()
+			if e.score != -checkmate {
+				panic(fmt.Sprintf("[%d] was not a checkmate %v", i, test.fen))
+			}
+		}
+	}
+}
+
+func BenchmarkMateIn2(b *testing.B) {
+	mateBenchmarker(b, 2*2-1, getTests(mates))
+}
+
+func BenchmarkMateIn3(b *testing.B) {
+	mateBenchmarker(b, 3*2-1, getTests(mates))
 }
